1c57fb9667262b5bd4dc72dcc624cc947b0d8551
Fabrizio Onorio <onoriofabrizio@gmail.com>
2021-12-03 14:57:05 +0100

    better code

--- 3rd/solutionDay3.js
+++ 3rd/solutionDay3.js
@@ -10,13 +10,13 @@ let epsilonRate = '';
 for(let n = 0; n < 12; n++) {
   let count0 = 0;
   let count1 = 0;
-  for(let i = 0; i < array.length - 1; i++) {
-    if(array[i].charAt(n) === '0'){
+  array.forEach( element => {
+    if(element.charAt(n) === '0'){
       count0++
     } else {
       count1++
     }
-  }
+  });
   if (count0 > count1) {
     gammaRate += '0';
     epsilonRate += '1';
@@ -32,107 +32,74 @@ let b = parseInt(epsilonRate, 2);
 console.log(a*b);
 
 // Part 2:
-
-  let oxygenGeneratorRating = [];
-  let co2ScraperRating = [];
-  let arrayOf0 = [];
-  let arrayOf1 = [];
-  for(let i = 0; i < array.length - 1; i++) {
-    if(array[i].charAt(0) === '0'){
-      arrayOf0.push(array[i])
-    } else {
-      arrayOf1.push(array[i])
-    }
-  }
-  if(arrayOf0.length > arrayOf1.length) {
-    for(let i = 0; i < arrayOf0.length; i++){
-      oxygenGeneratorRating.push(arrayOf0[i]);
-    }
-    for(let i = 0; i < arrayOf1.length; i++){
-      co2ScraperRating.push(arrayOf1[i]);
-    }
-  } else if(arrayOf1.length > arrayOf0.length) {
-      for(let i = 0; i < arrayOf1.length; i++){
-        oxygenGeneratorRating.push(arrayOf1[i]);
-      }
-      for(let i = 0; i < arrayOf0.length; i++){
-        co2ScraperRating.push(arrayOf0[i]);
-      }
+let oxygenGeneratorRating = [];
+let co2ScraperRating = [];
+let arrayOf0 = [];
+let arrayOf1 = [];
+array.forEach(element => {
+  if(element.charAt(0) === '0'){
+    arrayOf0.push(element)
   } else {
-      for(let i = 0; i < arrayOf1.length; i++){
-        oxygenGeneratorRating.push(arrayOf1[i]);
-
-      }
-      for(let i = 0; i < arrayOf0.length; i++){
-        co2ScraperRating.push(arrayOf0[i]);
-      }
+    arrayOf1.push(element)
   }
+});
+if(arrayOf0.length > arrayOf1.length) {
+  arrayOf0.forEach(element => { oxygenGeneratorRating.push(element) });
+  arrayOf1.forEach(element => { co2ScraperRating.push(element) });
+} else if(arrayOf1.length > arrayOf0.length) {
+    arrayOf1.forEach(element => { oxygenGeneratorRating.push(element) });
+    arrayOf0.forEach(element => { co2ScraperRating.push(element) });
+} else {
+    arrayOf1.forEach(element => { oxygenGeneratorRating.push(element) });
+    arrayOf0.forEach(element => { co2ScraperRating.push(element) });
+}
 
-
-
-  let n = 1;
+let n = 1;
 while(oxygenGeneratorRating.length > 1 ) {
-
   arrayOf0 = [];
   arrayOf1 = [];
-  for(let i = 0; i < oxygenGeneratorRating.length; i++) {
-    if(oxygenGeneratorRating[i].charAt(n) === '0'){
-      arrayOf0.push(oxygenGeneratorRating[i])
+  oxygenGeneratorRating.forEach( element => {
+    if(element.charAt(n) === '0'){
+      arrayOf0.push(element)
     } else {
-      arrayOf1.push(oxygenGeneratorRating[i])
+      arrayOf1.push(element)
     }
-  }
+  });
 
   oxygenGeneratorRating = [];
 
-
   if(arrayOf0.length > arrayOf1.length) {
-    for(let i = 0; i < arrayOf0.length; i++){
-      oxygenGeneratorRating.push(arrayOf0[i]);
-    }
+    arrayOf0.forEach( element => { oxygenGeneratorRating.push(element) });
   } else if(arrayOf1.length > arrayOf0.length) {
-      for(let i = 0; i < arrayOf1.length; i++){
-        oxygenGeneratorRating.push(arrayOf1[i]);
-      }
+      arrayOf1.forEach( element => { oxygenGeneratorRating.push(element) });
   } else {
-      for(let i = 0; i < arrayOf1.length; i++){
-        oxygenGeneratorRating.push(arrayOf1[i]);
-      }
+      arrayOf1.forEach( element => { oxygenGeneratorRating.push(element) });
   }
   n++;
 }
 
-   n = 1;
+n = 1;
 while(co2ScraperRating.length > 1 ) {
-
   arrayOf0 = [];
   arrayOf1 = [];
-  for(let i = 0; i < co2ScraperRating.length; i++) {
-    if(co2ScraperRating[i].charAt(n) === '0'){
-      arrayOf0.push(co2ScraperRating[i])
+  co2ScraperRating.forEach( element => {
+    if(element.charAt(n) === '0'){
+      arrayOf0.push(element)
     } else {
-      arrayOf1.push(co2ScraperRating[i])
+      arrayOf1.push(element)
     }
-  }
+  });
 
   co2ScraperRating = [];
 
-
   if(arrayOf0.length < arrayOf1.length) {
-    for(let i = 0; i < arrayOf0.length; i++){
-      co2ScraperRating.push(arrayOf0[i]);
-    }
+    arrayOf0.forEach( element => { co2ScraperRating.push(element) });
   } else if(arrayOf1.length < arrayOf0.length) {
-      for(let i = 0; i < arrayOf1.length; i++){
-        co2ScraperRating.push(arrayOf1[i]);
-      }
+      arrayOf1.forEach( element => { co2ScraperRating.push(element) });
   } else {
-      for(let i = 0; i < arrayOf0.length; i++){
-        co2ScraperRating.push(arrayOf0[i]);
-      }
+      arrayOf0.forEach( element => { co2ScraperRating.push(element) });
   }
   n++;
 }
-
 // solution 2:
 console.log(parseInt(oxygenGeneratorRating, 2) * parseInt(co2ScraperRating, 2))
